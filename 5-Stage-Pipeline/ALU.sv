39480b93f5c008b88f69e524638d224e15be2355
jaiden-stipp <business@jstipp.com>
2026-06-29 22:19:23 -0700

    Forwarding Implemented
    Created ForwardingUnit and Forward Mux, which forward the needed values to the ALU to be read in the EX stage. This resolves paths that may read an old register file value before it has been written to. Hazard Unit is next

--- 5-Stage-Pipeline/ALU.sv
+++ 5-Stage-Pipeline/ALU.sv
@@ -3,25 +3,25 @@ import pipeline_pkg::*;
 module ALU (
     input logic [31:0] A, B,
     input logic [3:0] ALU_Sel,
-    output logic [31:0] Q,
-    output logic Zero
+    output logic [31:0] ALU_result,
+    output logic ALU_Zero
 );
     always_comb begin
         case (ALU_Sel)
-            ADD:  Q = A + B;
-            SUB:  Q = A - B;
-            AND:  Q = A & B;
-            OR:   Q = A | B;
-            XOR:  Q = A ^ B;
-            SLTU: Q = (A < B);
-            SLT:  Q = ($signed(A) < $signed(B));
-            SLL:  Q = A << B[4:0];
-            SRL:  Q = A >> B[4:0];
-            SRA:  Q = $signed(A) >>> B[4:0];
-            PASSB: Q = B;
-            default: Q = 32'b0;
+            ADD:  ALU_result = A + B;
+            SUB:  ALU_result = A - B;
+            AND:  ALU_result = A & B;
+            OR:   ALU_result = A | B;
+            XOR:  ALU_result = A ^ B;
+            SLTU: ALU_result = (A < B);
+            SLT:  ALU_result = ($signed(A) < $signed(B));
+            SLL:  ALU_result = A << B[4:0];
+            SRL:  ALU_result = A >> B[4:0];
+            SRA:  ALU_result = $signed(A) >>> B[4:0];
+            PASSB: ALU_result = B;
+            default: ALU_result = 32'b0;
         endcase
     end
 
-    assign Zero = (Q == 32'b0);
+    assign ALU_Zero = (ALU_result == 32'b0);
 endmodule
--- 5-Stage-Pipeline/EX_MEM.sv
+++ 5-Stage-Pipeline/EX_MEM.sv
@@ -2,11 +2,11 @@ import pipeline_pkg::*;
 
 module EX_MEM(
     input logic clk, rst,
-    input logic [31:0] pc_EX, rs2_EX, imm_EX, Q_EX,
-    input logic Zero,
+    input logic [31:0] pc_EX, rs2_EX, imm_EX, ALU_result_EX,
+    input logic ALU_Zero,
     input ctrl_t ctrl_EX,
-    output logic [31:0] pc_MEM, rs2_MEM, imm_MEM, Q_MEM,
-    output logic Zero_MEM,
+    output logic [31:0] pc_MEM, rs2_MEM, imm_MEM, ALU_result_MEM,
+    output logic ALU_Zero_MEM,
     output ctrl_t ctrl_MEM
 );
     always_ff @(posedge clk) begin
@@ -14,15 +14,15 @@ module EX_MEM(
             pc_MEM <= 32'b0;
             rs2_MEM <= 32'b0;
             imm_MEM <= 32'b0;
-            Q_MEM <= 32'b0;
-            Zero_MEM <= 1'b0;
+            ALU_result_MEM <= 32'b0;
+            ALU_Zero_MEM <= 1'b0;
             ctrl_MEM <= '0;
         end else begin
             pc_MEM <= pc_EX;
             rs2_MEM <= rs2_EX;
             imm_MEM <= imm_EX;
-            Q_MEM <= Q_EX;
-            Zero_MEM <= Zero;
+            ALU_result_MEM <= ALU_result_EX;
+            ALU_Zero_MEM <= ALU_Zero;
             ctrl_MEM <= ctrl_EX;
         end
     end
